ce429981b0c4d3e181fae7d60efe665c72d04ac8
hamzashahid-10p <hamza.shahid@10pearls.com>
2024-01-18 13:59:48 +0500

    automate docker build and run commands

---
+++ README.md
@@ -0,0 +1,10 @@
+# Circle +W Advance IT Skill Program Class 5 Assignments
+
+## Task 1: Bash script to automate file/folder upload on github
+### Command to run:
+bash backend.sh
+
+## Task 2: Bash script to automate create, build, and run docker commands
+### Command to run:
+bash docker.sh
+
---
+++ docker.sh
@@ -0,0 +1,13 @@
+touch Dockerfile
+
+echo "FROM python:3" > Dockerfile
+echo "RUN pip3 install django" >> Dockerfile
+echo "COPY . ." >> Dockerfile
+echo "Run python manage.py migrate" >> Dockerfile
+echo "CMD ["python", "manage.py", "runserver", "0.0.0.0:8001"]" >> Dockerfile
+
+image_tag="django-app"
+
+sudo docker build -t "$image_name" .
+
+container_id=$(sudo docker run -p 8000:8000 -d "$image_name")
